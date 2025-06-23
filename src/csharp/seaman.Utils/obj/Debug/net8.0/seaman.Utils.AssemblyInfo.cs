33637d36064c1dfecc04d7e22ffbc7f2ad0d4fa7
unknown <simoneugomaria.bregaglio@crea.gov.it>
2025-06-23 17:08:47 +0200

    extension 2024 and various

--- src/csharp/seaman.Utils/obj/Debug/net8.0/seaman.Utils.AssemblyInfo.cs
+++ src/csharp/seaman.Utils/obj/Debug/net8.0/seaman.Utils.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("seaman.Utils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc66c126cecddf615789b8b7f182f429c2205a9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed3399831dc17a108a320c5ff20d9f90e15dbf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("seaman.Utils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("seaman.Utils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
